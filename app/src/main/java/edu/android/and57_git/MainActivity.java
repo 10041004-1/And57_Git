17e03146db87104efb031b18e68af8f065b99149
10041004-1 <starsj128@gmail.com>
2017-09-14 11:14:03 +0900

    bbbb가 올리겠슴돠 textView 멤버변수

--- app/src/main/java/edu/android/and57_git/MainActivity.java
+++ app/src/main/java/edu/android/and57_git/MainActivity.java
@@ -2,13 +2,19 @@ package edu.android.and57_git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
+
     }
 
 
